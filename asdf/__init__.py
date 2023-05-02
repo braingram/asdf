ae39cf368dc283f477fcf8e1378a74827d8a5200
Brett <brettgraham@gmail.com>
2023-05-02 17:35:27 -0400

    add ndarray converter (external blocks broken)

--- asdf/__init__.py
+++ asdf/__init__.py
@@ -24,6 +24,23 @@ from ._version import version as __version__
 from .asdf import AsdfFile
 from .asdf import open_asdf as open
 from .config import config_context, get_config
-from .stream import Stream
-from .tags.core import IntegerType
+from .tags.core import IntegerType, Stream
 from .tags.core.external_reference import ExternalArrayReference
+
+
+def __getattr__(name):
+    if name == "stream":
+        import warnings
+
+        import asdf.tags.core.stream
+        from asdf.exceptions import AsdfDeprecationWarning
+
+        warnings.warn(
+            "asdf.stream is deprecated. Please use asdf.tags.core.stream",
+            AsdfDeprecationWarning,
+        )
+
+        return asdf.tags.core.stream
+
+    msg = f"module {__name__!r} has no attribute {name!r}"
+    raise AttributeError(msg)
