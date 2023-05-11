6c061dcef8ddbea3f0fc19720eef05406c3e5d0e
Brett <brettgraham@gmail.com>
2023-05-11 13:37:30 -0400

    restore AsdfFile.version after write_to

--- asdf/asdf.py
+++ asdf/asdf.py
@@ -1262,10 +1262,14 @@ class AsdfFile:
             _handle_deprecated_kwargs(config, kwargs)
 
             if version is not None:
+                previous_version = self.version
                 self.version = version
 
             with generic_io.get_file(fd, mode="w") as fd:
                 self._serial_write(fd, pad_blocks, include_block_index)
+
+            if version is not None:
+                self.version = previous_version
 
     def find_references(self):
         """
