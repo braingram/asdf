c2d7e6b9e9cdf8112e682ec15c51b6164fddfb48
Brett <brettgraham@gmail.com>
2026-07-15 09:59:10 -0400

    simplify workflow

--- noxfile.py
+++ noxfile.py
@@ -278,7 +278,7 @@ def core(session: Session) -> None:
     Asdf(parallel=True).install(session).test(session)
 
 
-@nox.session(tags=["test", "coverage"], python="3.14")
+@nox.session(tags=["test", "core", "coverage"], python="3.14")
 def coverage(session: Session) -> None:
     """Run asdf test suite with coverage"""
     (
@@ -323,7 +323,7 @@ def compatibility(session: Session) -> None:
     Asdf(parallel=False).install(session, "virtualenv").test(session, "integration_tests/compatibility/")
 
 
-@nox.session(tags=["test"], python="3.12")
+@nox.session(tags=["test", "core"], python="3.12")
 def jsonschema(session: Session) -> None:
     """Run asdf jsonschema tests"""
     Asdf(parallel=False).install(session).test(session, "--jsonschema")
